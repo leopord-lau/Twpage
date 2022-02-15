e9b2ca7df01dfcfd9d66da0c4bbc4ff6b9123818
leo <2279846752@qq.com>
2022-02-15 22:24:05 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import TableWithPage from "./src/TableWithPage";
+import TPage from "./src/TPage";
+import TTable from "./src/TTable";
+import ResetScrollTop from "./src/ResetScrollTop";
+
+export {
+  TableWithPage,
+  TPage,
+  TTable,
+  ResetScrollTop
+}
